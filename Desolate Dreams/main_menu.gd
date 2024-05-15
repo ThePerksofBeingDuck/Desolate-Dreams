31265e8d6a25eeab8b594bf3f6acc90b7f2e966a
Duck <161622320+ThePerksofBeingDuck@users.noreply.github.com>
2024-05-14 21:05:43 -0600

    moar stuff

--- Desolate Dreams/main_menu.gd
+++ Desolate Dreams/main_menu.gd
@@ -4,11 +4,4 @@ extends Node2D
 @onready var settings_button = $Control/SettingsButton
 
 func _ready():
-	play_button.pressed.connect(_on_PlayButton_pressed)
-	settings_button.pressed.connect(_on_SettingsButton_pressed)
-
-func _on_PlayButton_pressed():
-	get_tree().change_scene("res://WorldSelection.tscn")
-
-func _on_SettingsButton_pressed():
-	get_tree().change_scene("res://Settings.tscn")
+	pass
