97a2dd871de230cfb106b6aeb850720625067286
Duck <161622320+ThePerksofBeingDuck@users.noreply.github.com>
2024-05-16 09:33:10 -0600

    idk what im doing at this point

--- Desolate Dreams/scripts/main_menu.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-@onready var play_button = $Control/PlayButton
-@onready var settings_button = $Control/SettingsButton
-
-func _ready():
-	pass
--- Desolate Dreams/scripts/settings.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-@onready var sound_slider = $SoundSlider
-@onready var brightness_slider = $BrightnessSlider
-@onready var text_size_slider = $TextSizeSlider
-
-func _ready():
-	# Here you should initialize the sliders with the current settings values
-	pass
