05c1dfb59f2f697541db448f7bbcf324c1bdf8aa
Duck <161622320+ThePerksofBeingDuck@users.noreply.github.com>
2024-05-26 19:02:28 -0600

    magic

--- Desolate Dreams Remake/scripts/CharacterBody2D.gd
+++ Desolate Dreams Remake/scripts/CharacterBody2D.gd
@@ -1,17 +1,21 @@
 extends CharacterBody2D
-
+#movement variables
 const SPEED = 100
+const SPRINT_SPEED = 200
 @onready var sprite = $Player
 
+#movement
 func _physics_process(_delta):
-	velocity.x = Input.get_axis("Walk Left", "Walk Right")
+	'''velocity.x = Input.get_axis("Walk Left", "Walk Right")
 	velocity.y = Input.get_axis("Walk Up", "Walk Down")
 	velocity = velocity.normalized()*SPEED
+	move_and_slide()'''
+	velocity.x = Input.get_axis("Walk Left", "Walk Right")
+	velocity.y = Input.get_axis("Walk Up", "Walk Down")
+	velocity = velocity.normalized() * SPEED
 	move_and_slide()
-	if Input.is_physical_key_pressed(KEY_SHIFT):
-		const SPEED = 350
-	else:
-		const SPEED = 200
+	
+	
 	if velocity.length() > 0:
 		if velocity.x > 0:
 			sprite.play("walk_right")
@@ -30,3 +34,33 @@ func _physics_process(_delta):
 			sprite.play("idle_up")
 		elif sprite.get_animation() == "walk_down":
 			sprite.play("idle_down")
+
+#magical moves
+
+#variables for magic
+
+var equipped_moves = {"1": null, "2": null}
+var move_fireball
+var move_firecrystal
+
+func _ready():
+	#loading moves
+	move_fireball = preload("res://scenes/Attacks/Fire/basic_fire_attack.tscn")
+	move_firecrystal = preload("res://scenes/Attacks/Fire/fire_crystal_attck.tscn")
+
+func _process(_delta):
+	if Input.is_action_just_pressed("Attack Slot 1"):
+		if equipped_moves["1"]:
+			trigger_move(equipped_moves["1"])
+
+	if Input.is_action_just_pressed("Attack Slot 2"):
+		if equipped_moves["2"]:
+			trigger_move(equipped_moves["2"])
+
+func trigger_move(move_scene):
+	var move_instance = move_scene.instantiate()
+	add_child(move_instance)
+	move_instance.global_position = global_position
+
+func equip_move(slot, move):
+	equipped_moves[slot] = move
